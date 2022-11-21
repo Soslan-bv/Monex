683be73095a8fbb61e81bb6f62019a2d686f0e2a
Byazrov_Soslan <soslan-bv@mail.ru>
2022-11-21 13:37:40 +0300

    'Monex'

---
+++ app/js/main.js
@@ -0,0 +1,9 @@
+$(function () {
+    $('.header__content-slider').slick({
+        dots: true,
+        arrows: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true
+    })
+})
